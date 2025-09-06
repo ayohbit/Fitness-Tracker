e9321a8a8f2b611c62391905c23fd2a0e8fd8e1a
Lucas Araújo <73756840+ayohbit@users.noreply.github.com>
2025-09-06 12:02:51 -0300

    criando segunda tela
    criaçao da segunda tela com formulario

--- app/src/main/java/com/androidexpress/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/androidexpress/fitnesstracker/MainActivity.kt
@@ -1,23 +1,28 @@
-
-
-
 package com.androidexpress.fitnesstracker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            // navegar para a proxima tela
+            val i = Intent(this, imcActivity::class.java)
+            startActivity(i)
         }
     }
 }
