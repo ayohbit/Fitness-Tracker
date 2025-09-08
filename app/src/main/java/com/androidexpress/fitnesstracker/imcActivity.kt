a9cb68e7f67d424f8c290d716255554edb7d61ef
Lucas Araújo <73756840+ayohbit@users.noreply.github.com>
2025-09-08 11:38:21 -0300

    aplicando logica de calculo de IMC
    aplicando logica de calculo de IMC

--- app/src/main/java/com/androidexpress/fitnesstracker/imcActivity.kt
+++ app/src/main/java/com/androidexpress/fitnesstracker/imcActivity.kt
@@ -1,20 +1,72 @@
 package com.androidexpress.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class imcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            try {
+                val weight = editWeight.text.toString().toInt()
+                val height = editHeight.text.toString().toInt()
+                val result = calculateImc(weight, height)
+                // mostrar IMC
+                //Toast.makeText(this, "IMC: $result", Toast.LENGTH_LONG).show()
+
+
+                val imcResponseId = imcResponse(result)
+                // mostrar condiçao da pessoa
+                Toast.makeText(this, getString(imcResponseId), Toast.LENGTH_SHORT).show()
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun imcResponse(imc: Double): Int {
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_low_weight
+            else -> R.string.imc_extreme_weight
+        }
+    }
+
+    private fun calculateImc(weight: Int, height: Int): Double {
+        return weight / ((height / 100.0) * (height / 100.0))
+    }
+
+    private fun validate(): Boolean {
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
     }
 }
